6b1615100d442a0fa5666e3f0c6d2eef76aeabf9
Rayane <rayanebarros@outlook.com>
2024-09-04 14:37:22 -0300

    Custom controls estilizados

--- js/slide.js
+++ js/slide.js
@@ -167,7 +167,7 @@ export class Slide {
   }
 }
 
-export class SlideNav extends Slide {
+export default class SlideNav extends Slide {
   constructor(slide, wrapper) {
     super(slide, wrapper);
     this.bindControlEvents();
